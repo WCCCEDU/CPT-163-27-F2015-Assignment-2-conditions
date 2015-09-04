a3596ce798b7512d6cda1e412c22e731503a3b5f
stevenPlas <steven.h.plas@gmail.com>
2015-09-04 10:00:30 -0400

    Java Assignment 2
    This is my first go at the assignment, it's not finished yet though. I
    have more for it.

---
+++ Assignment2Conditionals/src/assignment2conditionals/Assignment2Conditionals.java
@@ -0,0 +1,93 @@
+package assignment2conditionals;
+
+import java.util.Scanner;
+
+public class Assignment2Conditionals 
+{
+
+    public static void main(String[] args) 
+    {
+      Scanner scan = new Scanner (System.in);
+           
+     System.out.println("\t*************************************\n"
+                      + "\t*       Steve's Calculator          *\n"
+                      + "\t*************************************");
+          
+     System.out.println(" 1: Math\n 2: Subtraction\n 3: Multiplication"
+                     + "\n 4: Devision\n 5: Word Counter\n 6: Exit Program\n");
+     
+      System.out.print("Please Pick An Option: ");
+      int userInputNum = scan.nextInt();
+      System.out.print("\n");
+      
+      if(userInputNum == 1)
+      {
+        System.out.print("Please enter in your First variable: ");
+        double var1 = scan.nextDouble();
+        System.out.print("Please enter in your Second variable: ");
+        double var2 = scan.nextDouble();
+        double product = var1 + var2;
+        System.out.println("The answer is: " + product + "\n");
+      }
+      else if(userInputNum == 2)
+      {
+        System.out.print("Please enter in your First variable: ");
+        double var1 = scan.nextDouble();
+        System.out.print("Please enter in your Second variable: ");
+        double var2 = scan.nextDouble();
+        double product = var1 - var2;
+        System.out.println("The answer is: " + product + "\n");
+      }
+      else if(userInputNum == 3)
+      {
+        System.out.print("Please enter in your First variable: ");
+        double var1 = scan.nextDouble();
+        System.out.print("Please enter in your Second variable: ");
+        double var2 = scan.nextDouble();
+        double product = var1 * var2;
+        System.out.println("The answer is: " + product + "\n");
+      }
+      else if(userInputNum == 4)
+      {
+        System.out.print("Please enter in your First variable: ");
+        double var1 = scan.nextDouble();
+        System.out.print("Please enter in your Second variable: ");
+        double var2 = scan.nextDouble();
+        double product = var1 / var2;
+        System.out.println("The answer is: " + product + "\n");
+      }
+      
+      //Word Counter Program
+      else if(userInputNum == 5)
+      {
+        char L = 'L';
+        char O = 'O';
+        char V = 'V';
+        char E = 'E';
+        
+        int sum = (L + O + V + E);
+        
+        System.out.println("The word that I choose is \"Love\"\n");
+        
+        System.out.print("The Ascii value for L is ");
+        System.out.println((int)L);
+        System.out.print("The Ascii value for O is ");
+        System.out.println((int)O);
+        System.out.print("The Ascii value for V is ");
+        System.out.println((int)V);
+        System.out.print("The Ascii value for E is ");
+        System.out.println((int)E);
+        
+        System.out.println("\nThe sum of the Ascii values is: " + (int)(sum)
+                    + "\n");
+      }
+            
+      
+      else if(userInputNum == 6)
+      {
+          System.out.println("Goodbye!");
+         System.exit(0);
+      }
+    }
+    
+}
