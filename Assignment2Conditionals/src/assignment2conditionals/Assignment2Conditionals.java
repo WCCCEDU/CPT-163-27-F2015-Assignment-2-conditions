8e154df05365ba3e569db3300976ec71a0053603
stevenPlas <steven.h.plas@gmail.com>
2015-09-09 05:48:36 -0400

    Update # 2
    Taking some advice from my peers.

--- Assignment2Conditionals/src/assignment2conditionals/Assignment2Conditionals.java
+++ Assignment2Conditionals/src/assignment2conditionals/Assignment2Conditionals.java
@@ -13,15 +13,19 @@ public class Assignment2Conditionals
                       + "\t*       Steve's Calculator          *\n"
                       + "\t*************************************");
           
-     System.out.println(" 1: Math\n 2: Subtraction\n 3: Multiplication"
-                     + "\n 4: Devision\n 5: Word Counter\n 6: Exit Program\n");
+     System.out.println( "1: Math\n"
+                  + "2: Subtraction\n"
+                  + "3: Multiplication\n"
+                  + "4: Devision\n"
+                  + "5: Word Counter\n"
+                  + "6: Exit Program\n");
      
   
      System.out.print("Please Pick An Option: ");
      int userInputNum = scan.nextInt();
      System.out.print("\n");
       
-     
+  
      if(userInputNum == 1)
       {
         System.out.print("Please enter in your First variable: ");
@@ -31,8 +35,7 @@ public class Assignment2Conditionals
         double product = var1 + var2;
         System.out.println("The answer is: " + product + "\n");
       }
-      
-     else if(userInputNum == 2)
+      else if(userInputNum == 2)
       {
         System.out.print("Please enter in your First variable: ");
         double var1 = scan.nextDouble();
@@ -41,7 +44,6 @@ public class Assignment2Conditionals
         double product = var1 - var2;
         System.out.println("The answer is: " + product + "\n");
       }
-     
      else if(userInputNum == 3)
       {
         System.out.print("Please enter in your First variable: ");
@@ -51,34 +53,35 @@ public class Assignment2Conditionals
         double product = var1 * var2;
         System.out.println("The answer is: " + product + "\n");
       }
-     
      else if(userInputNum == 4)
       {
-        System.out.print("Please enter in your First variable: ");
-        double var1 = scan.nextDouble();
-        System.out.print("Please enter in your Second variable: ");
-        double var2 = scan.nextDouble();
-        double product = var1 / var2;
-        System.out.println("The answer is: " + product + "\n");
+         
+         System.out.print("Please enter in your Numerator: ");
+         double var1 = scan.nextDouble();
+         System.out.print("Please enter in your Denominator: ");
+         double var2 = scan.nextDouble();
+         if (var2 != 0)
+         {
+         double product = var1 / var2;
+         System.out.println("The answer is: " + product + "\n");
+         }
+         else
+             System.out.println("You can't divide by 0.");
       }
-      
       else if(userInputNum == 5)
       {
         wordCounter();
       }
-            
-      
       else if(userInputNum == 6)
       {
           System.out.println("Goodbye!");
          System.exit(0);
       }
-     
       else
           System.out.println("You did not enter a valid number.\n"
                   + "Goodbye!");
     }
-    //Word Counter Method
+//Word Counter Method
     public static void wordCounter()
     {
         
