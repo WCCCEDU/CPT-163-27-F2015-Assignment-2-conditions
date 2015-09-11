2c5ec3724634d19ed550fc1a8d663926e18f8fb4
Zachary Murphy <murphyz@my.wccc.edu>
2015-09-11 13:14:24 -0400

    1

---
+++ sept9/src/sept9/Sept9.java
@@ -0,0 +1,29 @@
+
+package sept9;
+
+
+public class Sept9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      /*  int i = 0;
+        while(i !=5){        //while i is not equal to fiev
+            System.out.println("before:" + i);
+            System.out.println(i+=2);
+            System.out.println("after:" + i);
+        }*/
+        int count =1;
+       while(count < 50){
+           if(count%5==0){
+           }else{
+               System.out.println(count);
+           }
+       
+
+           count++;
+       }
+        System.out.println(count);
+    }
+}
